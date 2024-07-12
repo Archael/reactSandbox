1909190b2a4614a8f1317cca86aa75fd528e10ef
Adam Evans <adam3van5@gmail.com>
2024-07-12 20:16:14 +0900

    challenge 25 solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,24 @@
 import Toast from './components/Toast';
 
+import { useState } from 'react';
+
 function App() {
+  const [enrolled, setEnrolled] = useState(false);
   function handleEnrol() {
     // Todo: Show toast
+    setEnrolled(true);
 
     setTimeout(() => {
       // Todo: hide toast
+      setEnrolled(false);
     }, 3000);
   }
 
   return (
     <div id="app">
       {/* Todo: Render <Toast /> component (conditionally) here */}
+      {enrolled && <Toast message="Enrolled Successfully" />}
+
       <article>
         <h2>React Course</h2>
         <p>
