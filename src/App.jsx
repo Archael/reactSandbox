f0c3c9ba547e4f67676abd76083348319ab80f65
Adam Evans <adam3van5@gmail.com>
2024-07-24 17:44:43 +0900

    exercise 27 useReducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,36 @@
-import Page from './components/page';
-import ThemeContextProvider from './store/context';
-import { useContext } from 'react';
-import { ThemeContext } from './store/context';
+import { useReducer } from 'react';
+
+export function counterReducer(state, action) {
+  if (action.type === 'INCREMENT') {
+    const updatedCount = state.count;
+    return { ...state, count: updatedCount + 1 };
+  }
+  if (action.type === 'DECREMENT') {
+    const updatedCount = state.count;
+    return { ...state, count: updatedCount - 1 };
+  }
+  if (action.type === 'RESET') {
+    return { ...state, count: 0 };
+  }
+}
 
 function App() {
-  const themeColor = useContext(ThemeContext);
+  const [counterState, counterDispatch] = useReducer(counterReducer, {
+    count: 0, // initial state
+  });
+  function CounterAction(action) {
+    counterDispatch({ type: action });
+  }
   return (
-    <ThemeContextProvider>
-      <Page themeColor={themeColor} />
-    </ThemeContextProvider>
+    <div id="app">
+      <h1>The (Final?) Counter</h1>
+      <p id="actions">
+        <button onClick={() => CounterAction('INCREMENT')}>Increment</button>
+        <button onClick={() => CounterAction('DECREMENT')}>Decrement</button>
+        <button onClick={() => CounterAction('RESET')}>Reset</button>
+      </p>
+      <p id="counter">{counterState.count}</p>
+    </div>
   );
 }
 
